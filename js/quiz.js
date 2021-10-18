b691656b39c54ad3facfd888598fb6c2474d96c6
Jose Uribe <joseuribeh@gmail.com>
2021-10-18 14:37:19 -0500

    Deploy - Links Updated (3)

--- js/quiz.js
+++ js/quiz.js
@@ -11,7 +11,7 @@ let availableQuestions = []
 
 let questions = []
 
-fetch("/questions.json")
+fetch("quiz-app/questions.json")
     .then( res => {
         return res.json()
     }).then( loadedQuestions => {
